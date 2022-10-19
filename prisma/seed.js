eb1bf0905589b51522435ff6dfe81bd3ff1b9bd4
Leo Ware <leoware@uni.minerva.edu>
2022-10-19 11:09:42 -0300

    prisma changes

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,25 +1,26 @@
-import { PrismaClient } from '@prisma/client'
+const {PrismaClient} = require('@prisma/client') // for some reason we're not an ES module
 const prisma = new PrismaClient()
 
-async function main(){
-  return {}
+async function main() {
+    seedTextBox()
+    return {}
+}
+
+const seedTextBox = async () => {
+    prisma.textBox.upsert({
+        where: {id: 0},
+        update: {},
+        create: {editorContent: '{"blocks":[{"key":"9lhsg","text":"Hello world!","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}'}
+    })
 }
 
 
 main()
     .then(async () => {
-      await prisma.$disconnect()
+        await prisma.$disconnect()
     })
     .catch(async (e) => {
-      console.error(e)
-      await prisma.$disconnect()
-      process.exit(1)
-    })main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
